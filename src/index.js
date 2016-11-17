69a7b55841814f073e4532ac92ad0d4c272563b0
andcastillo <andcastillo@gmail.com>
2016-11-16 21:54:20 -0500

    proxy intercept response

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+'use strict'
 /**
  * Created by acastillo on 11/10/16.
  */
+const proxy = require('express-http-proxy');
+const app = require('express')();
+
+app.use('/proxy', proxy('http://localhost:5000', {
+    intercept: function(rsp, data, req, res, callback) {
+        data = JSON.parse(data.toString('utf8'));
+        callback(null,JSON.stringify(data));
+    }
+}));
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
