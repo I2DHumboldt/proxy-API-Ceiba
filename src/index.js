206c01c366dceef6a6ca1d0cbc26c5923f2434f4
andcastillo <andcastillo@gmail.com>
2016-11-17 23:42:10 -0500

    proxy manage 2 groups of users: humboldt and guess

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 const proxy = require('express-http-proxy');
 var session = require('express-session');
 const app = require('express')();
+const _config = require('./config.json');
 
 var bodyParser = require('body-parser');
 
@@ -23,28 +24,40 @@ app.get('/',function(req,res){
     liveSession = req.session;
     //Session set when user Request our app via URL
     if(liveSession.email) {
-        res.render('index.html');
+        res.render(_config.login_redirect);
     }
     else {
-        res.render('login.html');
+        res.render(_config.login);
     }
 });
 
-app.use('/proxy', proxy('http://localhost:5000', {
+app.use('/proxy', proxy(_config.api_server, {
     reqAsBuffer: true,
     decorateRequest: function(proxyReq, originalReq) {
         proxyReq.headers['Content-Type'] = 'application/json';
         proxyReq.headers['accept'] = 'application/json';
+        //Avoid clients could fool my system
+        if(proxyReq.path.indexOf('&group=')) {
+            proxyReq.path.replace(/&group=/g, "&groupp=")
+        }
+        if(liveSession && liveSession.group)
+            proxyReq.path+='&group='+liveSession.group;
+        else
+            proxyReq.path+='&group=guess';
         return proxyReq;
     },
-
     intercept: function(rsp, data, req, res, callback) {
-        if(liveSession && liveSession.group && liveSession.group === "humboldt"){
+        if(liveSession && liveSession.group) {
+            data = JSON.parse(data.toString('utf8'));
+            callback(null, JSON.stringify(data));
+        }
+
+        /*if(liveSession && liveSession.group && liveSession.group === "humboldt"){
             data = JSON.parse(data.toString('utf8'));
             callback(null, JSON.stringify(data));
         } else {
             callback(null,"Sorry, you are not a humboldt user");
-        }
+        }*/
 
     }
 }));
@@ -61,22 +74,34 @@ app.post('/login/ceiba/login.do',function(req,res){
     liveSession = req.session;
     //In this we are assigning email to sess.email variable.
     //email comes from HTML page.
-    console.log(req.body);
-    if (req.body.password === "123"){
-        console.log(req.body.email);
-        console.log(req.body.email.indexOf("@humboldt.org.co"));
-        if (req.body.email.indexOf("@humboldt.org.co")>=0) {
-            liveSession.group = "humboldt";
-
-        } else {
-            liveSession.group = "guess";
-        }
-        liveSession.email=req.body.email;
-        res.end('done as '+liveSession.group);
+    if(req.body.email === "guess"){
+        liveSession.group = "guess";
+        res.end('Logged in as '+liveSession.group);
     }
-    res.end("Failed to login");
+    else{
+        if (req.body.email.indexOf("@humboldt.org.co")>=0) {
+            if (req.body.password === "123"){
+                liveSession.group = "humboldt";
+                liveSession.email=req.body.email;
+                res.end('Logged in as '+liveSession.group);
+            }
+        }
+    }
+
+    res.end("login failed");
 });
 
-app.listen(3000, function () {
+app.get('/logout',function(req,res){
+    req.session.destroy(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+
+});
+
+app.listen(_config.port, function () {
     console.log('Example app listening on port 3000!')
 });
